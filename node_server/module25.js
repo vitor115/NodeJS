33abf02aec94f72c5d16c43650ffd0c485fa18ce
Vitor115 <vitorravacci@yahoo.com.br>
2019-03-13 14:19:55 -0300

    adicao de modulos e arquivos

--- node_server/module25.js
+++ node_server/module25.js
@@ -1,4 +1,4 @@
-// Express App 25
+// Express App 25 and 26
 
 var express = require('express');
 
@@ -6,15 +6,16 @@ var app = express();
 app.set('view engine', 'ejs'); //usando EJS como ferramenta para visualização para "visualizar" o perfil requisitado
 
 app.get('/', function(req,res){
-    res.sendFile(__dirname + '/index.html');
+    res.render('index');
 });
 
 app.get('/contact', function(req,res){ //Enviando HTML atraves do sendFile
-    res.sendFile(__dirname +'/contact.html');
+    res.render('contacts');
 });
 
 app.get('/profile/:name', function(req, res){
-    res.send('You are viewing the profile of ' + req.params.name); // Ele recebe parametros a partir da URL e responde a ele dizendo que parametro ele pediu
+    var data = {age: 18, job: 'Developer', hobbies: ['eating', 'fishing', 'sleeping']};
+    res.render('profile',{person: req.params.name , data: data}); // Ele recebe parametros a partir da URL e responde a ele dizendo que parametro ele pediu
 });
 
 
